ae2d666c64181b66200e75d3144807636ef03845
shashank <shashankofficial3209@gmail.com>
2023-09-09 15:26:57 +0530

    pwa-edit-1

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-// next.config.js
-const withOffline  = require('next-offline');
+const withOffline = require('next-offline');
+const withPWA = require('next-pwa');
 
-module.exports = withOffline({
-  generateInDevMode: true, // Set to true if you want to enable service workers in development
-  skipWaiting: true, // Set to true to skip the waiting phase when a new service worker is activated
-  // Other Next.js configuration options
-  // ...
-});
+module.exports = withOffline(
+  withPWA({
+    generateInDevMode: true, // Set to true if you want to enable service workers in development
+    skipWaiting: true, // Set to true to skip the waiting phase when a new service worker is activated
+    pwa: {
+      dest: 'public', // The directory where the service worker file will be placed
+      register: true, // Enable service worker registration
+      sw: 'service-worker.js', // The path to your custom service worker file
+    },
+    // Other Next.js configuration options
+    // ...
+  })
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,15 +28,16 @@ export default function MyApp(props: MyAppProps) {
   const getLayout = Component.getLayout ?? ((page) => page);
   React.useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js') // Path to your service worker file
-        .then((registration) => {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
           console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
+        }).catch((error) => {
           console.error('Service Worker registration failed:', error);
         });
+      });
     }
+    
+    
   }, []);
   return (
     <CacheProvider value={emotionCache}>
